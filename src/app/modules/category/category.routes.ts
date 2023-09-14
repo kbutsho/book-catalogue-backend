173b63f74fc1d216a066b5fa0126945ccca8fabe
Kaushik Biswas <kbutsho@gmail.com>
2023-09-14 21:24:29 +0600

    category route added

---
+++ src/app/modules/category/category.routes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { CategoryController } from './category.controller';
+import { CategoryValidation } from './category.validation';
+
+const router = express.Router();
+
+router.get('/', CategoryController.getAllCategory);
+router.post('/create-category', auth(ENUM_USER_ROLE.ADMIN), validateRequest(CategoryValidation.createCategory), CategoryController.createCategory);
+router.get('/:id', CategoryController.categoryById);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), CategoryController.updateCategory);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), CategoryController.deleteCategory);
+
+export const categoryRoutes = router;
