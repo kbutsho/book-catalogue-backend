41f7c4e9602a2ab18219b770bc7c9797f430c474
Kaushik Biswas <kbutsho@gmail.com>
2023-09-19 18:43:10 +0600

    category routes updated

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -8,9 +8,9 @@ import { CategoryValidation } from './category.validation';
 const router = express.Router();
 
 router.get('/', CategoryController.getAllCategory);
-router.post('/create-category', auth(ENUM_USER_ROLE.ADMIN), validateRequest(CategoryValidation.createCategory), CategoryController.createCategory);
+router.post('/create-category', auth(ENUM_USER_ROLE.admin), validateRequest(CategoryValidation.createCategory), CategoryController.createCategory);
 router.get('/:id', CategoryController.categoryById);
-router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), CategoryController.updateCategory);
-router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), CategoryController.deleteCategory);
+router.patch('/:id', auth(ENUM_USER_ROLE.admin), CategoryController.updateCategory);
+router.delete('/:id', auth(ENUM_USER_ROLE.admin), CategoryController.deleteCategory);
 
 export const categoryRoutes = router;
