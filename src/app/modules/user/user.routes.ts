14a1d987e4deb5d7b46dc02dea66efd41ce830e5
Kaushik Biswas <kbutsho@gmail.com>
2023-09-19 18:43:26 +0600

    user routes updated

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -4,9 +4,9 @@ import { ENUM_USER_ROLE } from './../../../enums/user';
 import { userController } from './user.controller';
 const router = express.Router();
 
-router.get('/', auth(ENUM_USER_ROLE.ADMIN), userController.geAllUser);
-router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.getSingleUser);
-router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.updateUser);
-router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), userController.deleteUser);
+router.get('/', auth(ENUM_USER_ROLE.admin), userController.geAllUser);
+router.get('/:id', auth(ENUM_USER_ROLE.admin), userController.getSingleUser);
+router.patch('/:id', auth(ENUM_USER_ROLE.admin), userController.updateUser);
+router.delete('/:id', auth(ENUM_USER_ROLE.admin), userController.deleteUser);
 
 export const userRoutes = router;
