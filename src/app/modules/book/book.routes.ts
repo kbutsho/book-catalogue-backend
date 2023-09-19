a1f9813e30894e2ed6ed76d92f3940f5c52c5a89
Kaushik Biswas <kbutsho@gmail.com>
2023-09-19 18:42:23 +0600

    book routes added

---
+++ src/app/modules/book/book.routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+import validateRequest from '../../middlewares/validateRequest';
+import { BookController } from './book.controller';
+import { bookValidation } from './book.validations';
+
+const router = express.Router();
+
+router.get('/', BookController.getAllBook);
+router.post('/create-book', auth(ENUM_USER_ROLE.admin), validateRequest(bookValidation.createBook), BookController.createBook);
+router.get('/:categoryId', BookController.gateBooksByCategoryId);
+router.get('/:id', BookController.getBookById);
+router.patch('/:id', auth(ENUM_USER_ROLE.admin), BookController.updateBook);
+router.delete('/:id', auth(ENUM_USER_ROLE.admin), BookController.deleteBook);
+
+export const bookRoutes = router;
